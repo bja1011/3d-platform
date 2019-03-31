81d066ff9ab5e37c578d82ac282fada699d735a9
Adam Kaczmarek <bja1011@gmail.com>
2019-04-01 00:57:22 +0200

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    private Vector3 moveDirection;
+    private CharacterController controller;
+    public float gravityScale;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale;
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
