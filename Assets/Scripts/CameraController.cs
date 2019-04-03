01a4786811e054c858fc6d20f604650be0d51d9c
Adam Kaczmarek <bja1011@gmail.com>
2019-04-03 23:55:57 +0200

    control camera and player with mouse

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,16 +6,39 @@ public class CameraController : MonoBehaviour
 {
     public GameObject target;
 	public Vector3 offset;
+    public float rotateSpeed = 5;
+    public Transform pivot;
 
     void Start()
     {
         offset = target.transform.position - transform.position;
+
+        pivot.position = target.transform.position;
+        pivot.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-    	transform.position = target.transform.position - offset;
+
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+
+        target.transform.Rotate(0, horizontal, 0);
+        pivot.Rotate(-vertical, 0, 0);
+
+
+        float desiredYAngle = target.transform.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+
+        transform.position = target.transform.position - (rotation * offset);
+
+    	//transform.position = target.transform.position - offset;
         //transform.LookAt(new Vector3(target.transform.position.x,0f,target.transform.position.z));
+        transform.LookAt(target.transform);
     }
 }
---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float turnSpeed = 5;
+    private Vector3 moveDirection;
+    private CharacterController controller;
+    public float gravityScale;
+
+    private bool superJumpUsed;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        moveDirection.y = moveDirection.y + Physics.gravity.y * gravityScale * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
